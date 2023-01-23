fc70241bddc3ad725ba6be91875c91801e683a65
Aniraj1 <anirajshahi@gmail.com>
2023-01-23 09:01:39 +0545

    Repository changes

--- ApexResturant.Repository/RepositoryModule.cs
+++ ApexResturant.Repository/RepositoryModule.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ApexResturant.Repository.RCustomer;
 
+
 namespace ApexResturant.Repository
 {
     public static class RepositoryModule
